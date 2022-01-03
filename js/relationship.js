a82851ff89b7b09dfae9f38c987880f83b582b43
Libo Sun <sunlibo@yahoo.com>
2022-01-02 19:30:58 -0600

    first commit

---
+++ js/relationship.js
@@ -0,0 +1,264 @@
+var svg = d3.select('#relationshipWeb')
+    .append('svg')
+    .attr('width', WIDTH)
+    .attr('height', HEIGHT)
+    .attr('margin-top', 30);
+
+//define GE as first node (she's not in the big Excel sheet)
+var nodes = [
+    {
+        "Radius": 10 * 4,
+        "Closeness": 10 * 4,
+        "index": 0,
+        "ImagePath": "image_0"
+    }
+];
+var links = [];
+
+//list for people datalist
+var people = [];
+
+//create def to group images
+var images = svg.append("defs").attr('id', 'images');
+
+//set GE's image
+setImage('images/png/georgeeliot.png', 0);
+
+//set default image for individuals with no pictures
+setImage('images/png/nopicture_friend.png', 'friend');
+setImage('images/png/nopicture_acquaintence.png', 'acquaintance');
+setImage('images/png/nopicture_family.png', 'family');
+
+
+//Scale Legend
+var maxRadius = 20;
+const scaleLegendGroup = svg.append('g')
+                            .attr('class', 'scale-legend')
+.attr('transform', `translate(${WIDTH *2 /5}, 25)`)
+                            .attr('opacity', 0.7);
+const scaleLegend = d3.scaleLinear();
+
+//setup scale Legend 
+const legendSize = d3.legendSize()
+                        .scale(scaleLegend)
+                        .shape('circle')
+                        .title('Size Legend')
+                        .shapePadding('12')
+                        .labelOffset(20)
+                        .orient('horizontal')
+                        .labels(["Less-Close",
+                                "",
+                                "",
+                                "",
+                                "More-Close"])
+                        .labelWrap(30)
+                        .shapeWidth(40)
+                        .labelAlign('start')
+                        
+
+
+/**
+ * Parse csv data
+ */
+var parseData = d3.csv('data/ge_people.csv', function(node) {
+    if(node) {   //only parse important individuals
+        node = parseFullName(node);
+
+        //give index to node
+        node = setIndex(node);
+        node['nodeId'] = 'node_' + node['index'];
+        
+        //parse closeness into a number
+        let closeness = parseInt(node.Closeness);
+        if(closeness < 0) {
+            node.Closeness = 0;
+            node.Radius = 0;
+        } else {
+            node.Radius = (17 - closeness) * 2;
+            node.Closeness = closeness + 2;
+        }
+
+        //push current person to list of people for datalist
+        var person = new Object();
+        person.name = node['FullName'];
+        person.id = node['nodeId'];
+        people.push(person);
+
+        //set main and secondary colors
+        switch(node.Relationship) {
+            case 'friend':
+                node.mainColor = FRIEND_COLOR;
+                node.secondaryColor = FRIEND_SECONDARY_COLOR;
+                break;
+            case 'business associate':
+                node.mainColor = ACQ_COLOR;
+                node.secondaryColor = ACQ_SECONDARY_COLOR;
+                break;
+            default:
+                node.mainColor = FAMILY_COLOR;
+                node.secondaryColor = FAMILY_SECONDARY_COLOR;
+                break;
+        }
+
+        if(node.Image) {
+            setImage('images/png/' + node.Image, node.index);
+            node.ImagePath = 'image_' + node.index
+        } else {
+            //set individuals with no images to defaults
+            switch(node.Relationship) {
+                case 'friend':
+                    node.ImagePath = 'image_friend';
+                    break;
+                case 'family':                
+                    node.ImagePath = 'image_family';
+                    break;
+                default:
+                    node.ImagePath = 'image_acquaintance';
+                    break;
+            }
+        }
+        nodes.push(node);
+        links.push({source: node.index, target: 0});
+    }
+});
+
+/**
+ * Populates the dropdown for the search functionality and starts a force simulation using nodes data
+ */
+parseData.then(function() {
+    //sort and append people data to the datalist
+    people.sort(function(a, b) {
+        if(a.name > b.name) {
+            return 1;
+        } else if (b.name > a.name) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+    var peopleList = document.getElementById('people');
+
+    people.forEach(function(person) {
+        var htmlOption = document.createElement('option');
+        htmlOption.value = person.name;
+        peopleList.appendChild(htmlOption);
+    })
+    
+    
+
+    //create a grouping for nodes
+    var nodeGroup = svg.append('g').attr('id', 'nodes');
+    //append a circle to group
+    nodeGroup.selectAll('g')
+        .data(nodes)
+        .enter()
+        .append('g')
+        .attr('id', function(d) {return d['nodeId']})
+        
+    var node = nodeGroup.selectAll('g').append('circle')
+        .attr('class', 'RelationshipNode')
+        .attr('r', function(d) {return d.Radius}) 
+        //fill with image
+        .attr('loading', 'lazy')
+        .attr('fill', function(d) {
+            return 'url(#' + d.ImagePath + ')';
+         })
+         .attr('opacity', 0.0)
+            
+        //  .attr('onload', function(d){
+        //     images.attr('fill', 'url(#' + d.ImagePath + ')')
+        //  })
+        
+         //add border color
+        .attr('stroke', function(d){
+            if(d.mainColor) {
+                return d.mainColor;
+            } else {
+                return GE_COLOR;
+            }
+        })
+        .attr('stroke-width', function(d) {
+            return d.Radius / 10;
+        })
+        .on('mouseover', function() {
+            selectNode(d3.select(this));
+        })
+        .on('mouseout', function() {
+            deselectNode(d3.select(this));
+        })
+        .on('click', function() {
+            showSummary(d3.select(this));
+        });
+        node.transition()
+        .duration(3000)
+        .attr('opacity', 1);
+        // .attr('fill', function(d) { return 'url(#' + d.ImagePath + ')'; })
+
+
+    //scale legend domain
+    // scaleLegend.domain(d3.extent(node, function(d) {
+    //     console.log(node)
+    //     return d.Closeness}))
+    //             .range([10, maxRadius])
+    scaleLegend.domain([0, 50])
+                .range([10, maxRadius])
+
+    //setup size legend
+    scaleLegendGroup.call(legendSize)
+    scaleLegendGroup.selectAll('text')
+                    .attr('fill', 'white')
+                    .attr('stroke-width', 0)
+                    .style('font-family', 'monospace')
+                    .style('font-size', '14px')
+    scaleLegendGroup.select('g')
+                    .attr('fill', 'rgba(231, 220, 67, 0.5)')
+                    .attr('stroke', 'white')
+                    .attr('stroke-width', 2)
+    
+     
+
+    //set distance between nodes
+    var NODE_DISTANCE = 30;
+    var linkForce = d3.forceLink(links)
+        .distance(function(d) {
+            return  d.source.Closeness * NODE_DISTANCE;
+        })
+        .id(function(d) {return d.index; });
+
+     //start simulation
+    //  d3.forceSimulation(nodes)
+    //  .force('charge', d3.forceManyBody())
+    //  .force('center', d3.forceCenter(WIDTH/2, HEIGHT/2))
+    //  .force('links', linkForce)
+    //  .force('collision', d3.forceCollide().radius(function(d) {
+    //     return d.Radius + (d.Radius/10);  //account for stroke
+    //   }))
+    //  .on('tick', function() {
+    //      node
+    //          .attr('cx', function(d) {
+    //             return (d.x = Math.max(d.Radius, Math.min(WIDTH - d.Radius, d.x)));   //prevents exiting boundaries
+    //           })
+    //           .attr('cy', function(d) {
+    //             return (d.y = Math.max(d.Radius, Math.min(HEIGHT - d.Radius, d.y)));  //prevents exiting boundaries
+    //           })
+    //  });
+    d3.forceSimulation(nodes)
+        .force('charge', d3.forceManyBody().strength(5))
+        .force('center', d3.forceCenter(WIDTH/2, HEIGHT/2-50))
+        .velocityDecay(0.5)
+        .force("x", d3.forceX().strength(0.03))
+        .force("y", d3.forceY().strength(0.3))
+        .force('links', linkForce)
+        .force("collide", d3.forceCollide().radius(d => (d.Radius+ d.Radius/10)).iterations(50))
+        .on('tick', function() {
+            node
+            .attr('cx', function(d) {
+                return (d.x = Math.max(d.Radius, Math.min(WIDTH - d.Radius, d.x)));   //prevents exiting boundaries
+              })
+            .attr('cy', function(d) {
+                return (d.y = Math.max(d.Radius, Math.min(HEIGHT - d.Radius, d.y)));  //prevents exiting boundaries
+              })
+            });
+
+        
+});
---
+++ js/relationshipsUtils.js
@@ -0,0 +1,393 @@
+/**
+ * Global variables
+ */
+
+ //colors for node backgrounds based on relationship type
+ var FRIEND_COLOR = '#93c6b1';
+ var FRIEND_SECONDARY_COLOR = '#def7ed';
+ 
+ var FAMILY_COLOR = '#ff9bb2';
+ var FAMILY_SECONDARY_COLOR = '#ffe8ed';
+ 
+ var ACQ_COLOR = '#fff09b';
+ var ACQ_SECONDARY_COLOR = '#fffbe8';
+ 
+//  var GE_COLOR = '#636363';
+// var GE_COLOR = '#bd874a';
+
+var GE_COLOR = '#DAA520';
+ 
+ var SEARCHED_NODE_COLOR = '#701313';
+ 
+ //values for sizing elements
+ var MAGNIFYING_RATIO = 1.5;
+ var MARGIN = 20;
+//  var WIDTH = document.getElementById("relationshipWeb").offsetWidth;
+//  var HEIGHT = document.getElementById("relationshipWeb").offsetHeight;
+var WIDTH = 1000;
+var HEIGHT = 900;
+var USER_MENU_WIDTH_SHOW = '320px';
+var USER_MENU_WIDTH_HIDE = '160px'
+ 
+ var  IMAGE_R = 80; //radius for biography container
+
+
+ var USER_MENU_SHOWN = true;
+ 
+ /**
+  * Creates a "pattern" element with the given imagePath and sets the pattern id with the index of the individual.
+  * 
+  * @param {string} image 
+  * @param {int} index 
+  */
+ function setImage(imagePath, index) {
+     images.append('pattern')
+         .attr('id', 'image_' + index)
+         .attr("width", 1)
+         .attr("height", 1)
+         .attr("viewBox", "0 0 100 100") //scalable image
+     .append("svg:image")                    //link image
+        // .attr('onload', function(){
+        //     images.attr("fill", "url(#" + imagePath + ")")            
+        // })
+         .attr("xlink:href", imagePath)
+         .attr("width", 100)
+         .attr("height", 100)
+         .attr("y", 0)
+         .attr("x", 0);
+ }
+ 
+ /**
+  * returns a random number between min and max. Used only for generating dummy data.
+  * @param {double} min 
+  * @param {double} max 
+  */
+ function randomRange(min, max) {
+     return Math.random() * (max - min) + min;
+ }
+ 
+ /**
+  * Returns a string combining all the names of the individual.
+  * Title FirstName "Nickname" SecondNames LastName
+  * @param {JSON object} node 
+  */
+ function parseFullName(node) {
+              //parse name
+     var name = node["First Name"];
+ 
+     if(node["Nickname"]) {
+         name += " \"" + node["Nickname"] + "\"";
+     }
+     if (node["Second Names"]) {
+         name += " " + node["Second Names"];
+     }
+ 
+     name += " " + node["Last Name"];
+ 
+     // if(node["Title"]) {
+     //     name = node["Title"] + " " + name;
+     // }
+ 
+     node["FullName"] = name;
+     return node;
+ }
+ 
+ /**
+  * Called by a mouseover event on a node. 
+  * Brings the hovered node to the front, magnifies it by the MAGNIFYING_RATIO, and attaches the individual's name
+  * with the id #currentText to the top.
+  * @param {D3 Selection} node 
+  */
+ function selectNode(node) {
+    node.attr('r', function(d) { return d.Radius});
+     var data = node.data()[0];
+ 
+     //check that it is not GE
+     if(data.index == 0) {
+         return;
+     }
+
+     //get parent node
+     var parent = d3.select(node.node().parentNode);
+     //check that its "searched" name tag isn't up
+     console.log(parent._groups[0][0].childElementCount)
+     if(parent._groups[0][0].childElementCount < 2) {
+         //values from node
+        
+        var r = node.attr('r');
+        //offset r from x and y to prevent overlap with image
+        var x = node.attr('cx') - r; 
+        var y = node.attr('cy') - r;
+    
+        //magnify node
+        node.attr('r', r * MAGNIFYING_RATIO);
+        // node.transition()
+        //     .duration(100)
+        //     .attr('r', r * MAGNIFYING_RATIO);
+        
+    
+    
+        parent.moveToFront();
+    
+        var group = parent.append('g')
+                        .attr('id', 'currentText');
+
+        // var x = group.append('body')
+
+        var f = group.append('foreignObject')
+            .attr('x', x - 30)
+            .attr('y', y + data.Radius/2*4)
+            .attr('width', '120px')
+            .attr('height', '55px')            
+            .attr('opacity', 0.1)   
+                            
+            .append('xhtml:div')
+            .html(data['FullName'])            
+            .attr('class', 'fullNameNode')
+            .attr('class', function() {
+                switch(data['Relationship']) {
+                    case 'friend':
+                        return "fullNameBorderFriend";
+                    case 'family':
+                        return 'fullNameBorderFamily';
+                    default:
+                        return 'fullNameBorderBA';
+                }
+            })
+            
+            group.selectAll('foreignObject')
+            .transition()
+            .duration(500)            
+            .attr('opacity', 0.8)
+            
+            
+            
+     }
+ 
+     
+ }
+
+ /**
+  * Removes the D3 selections with the id #currentText and resets the radius to the original radius.
+  * @param {D3 Selection} node 
+  */
+ function deselectNode(node) {
+     //remove name
+     d3.select('#currentText').remove();
+     //revert node size
+     node.attr('r', function(d) { return d.Radius});
+    //  node.transition()
+    //         .duration(500)
+    //         .attr('r', node.attr('r'));
+
+
+ }
+ 
+ /**
+  * Appends a small svg containing biography information to the main svg with the id #currentSummary
+  * @param {D3 Selection} node
+  */
+
+ function showSummary(node) {
+    var data = node.data()[0];
+
+    //check that it is not GE
+    if(data.index == 0) {
+        return;
+    }
+    
+    clearDisplay();
+
+    if(!USER_MENU_SHOWN) {
+        toggleMenu();
+    }
+
+    document.getElementById('Biography').style.display = "block";
+
+    switch(data.Relationship) {
+        case "family":
+            document.getElementById('Biography').classList.add("BiographyFamily");
+            break;
+        case "friend":
+            document.getElementById('Biography').classList.add("BiographyFriend");
+            break;
+        default: 
+            document.getElementById('Biography').classList.add("BiographyBA");
+            break;
+    }
+    document.getElementById('CloseButton').style.display = "block";
+    d3.select('#BiographyName').append('text').text(data['FullName']);
+    d3.select('#BirthAndDeath').append('text').text(getBirthDeathDates(data['Birth'], data['Death']));    
+    d3.select('#BiographySummary').append('text').html(handleBiographyText(data['Biography'])); 
+    if(data.Image) {
+        document.getElementById('BiographyPhoto').src = "images/png/" + data.Image;
+    }
+    if(data['Letters']) {
+        d3.select('#BiographyLetters').append('text').html("<u>Letters from <i>George Eliot letters</i>, vols 1-9, edited by Gordon Haight (Yale UP, 1954-1978):</u></br>")
+        d3.select('#BiographyLetters').append('text').html(data['Letters']);
+    }
+}
+ 
+ /**
+  * If there is no biography present for an indual, the function returns 'No data'.
+  * @param {string} biography 
+  */
+ function handleBiographyText(biography) {
+     return biography? biography : 'No data';
+ }
+ 
+ /**
+  * Removes the element with the id 
+  */
+ function clearDisplay() {
+    document.getElementById('CloseButton').display = "none";
+    document.getElementById('Biography').classList.remove("BiographyFamily");
+    document.getElementById('Biography').classList.remove("BiographyFriend");
+    document.getElementById('Biography').classList.remove("BiographyBA");
+    document.getElementById('BiographyPhoto').src = '';
+    document.getElementById('BiographyName').innerHTML = '';
+    document.getElementById('BirthAndDeath').innerHTML = '';
+    document.getElementById('BiographySummary').innerHTML = '';
+    document.getElementById('BiographyLetters').innerHTML = '';
+    document.getElementById('Biography').style.display = "none";
+ }
+ 
+ /**
+  * Returns a string concatenating the birth and death dates.
+  * If there is no data for either of the fields, the string 'Unknown' is used instead.
+  * @param {string} birth 
+  * @param {string} death 
+  */
+ function getBirthDeathDates(birth, death) {
+     if(birth && death) {
+         return birth + " - " + death;
+     }
+ }
+ 
+ /**
+  * Sets the index of every node. Uses closure to maintain integrity of the index, even with an asynchronous function.
+  */
+ var setIndex = (function(node) {
+     var index = 1;
+     return function(node) {
+         node.index = index;
+         index++;
+         return node;
+     }
+ })();
+ 
+ /**
+  * Moves the selection to the front of the DOM
+  */
+ d3.selection.prototype.moveToFront = function() {
+     return this.each(function(){
+         this.parentNode.appendChild(this);
+     });
+ };
+ 
+ /**
+  * "selects" nodes that match the searchTerm
+  */
+ function filterInput() {
+     //remove all currently shown
+     d3.selectAll('.searchedText').remove();
+ 
+     var searchTerm = document.getElementById('searchBar').value;
+     //trim and lowercase searchterm
+     searchTerm = searchTerm.trim().toLowerCase();
+     if(searchTerm.length > 4) {
+         var indicies = getIndiciesOfSearchTerm(searchTerm);
+ 
+         indicies.forEach(function(index) {
+             var nodeId = people[index].id;
+             selectSearchedNode(d3.select('#' + nodeId));
+         });
+     }
+ }
+ 
+ /**
+  * returns an array of all nodes with names containing searchTerm
+  * @param {string} searchTerm 
+  */
+ function getIndiciesOfSearchTerm(searchTerm) {
+     var indicies = [];
+     var index = -1;
+     people.forEach(function(person) {
+         //compare against lowercased name
+         if(person.name.toLowerCase().includes(searchTerm)) {
+             index = people.map(function(e) { return e.name; }).indexOf(person.name);
+             indicies.push(index);
+         }
+     });
+     return indicies;
+ }
+ 
+ /**
+  * Similar to the selectNode function, this function "selects" nodes by bringing the 
+  * selected node to the front, magnifying it, and attaching the name.
+  * @param {D3 Selection} node 
+  */
+ function selectSearchedNode(node) {
+     var data = node.data()[0];
+ 
+     //values from node
+     var r = node._groups[0][0].firstChild.r.animVal.value;
+     //offset r from x and y to prevent overlap with image
+     var x = data['x'];
+     var y = data['y'];
+
+     //get parent node
+     var parent = d3.select(node.node().parentNode);
+ 
+     parent.moveToFront();
+ 
+     var group = node.append('g')
+                     .attr('class', 'searchedText');
+ 
+    group.append('foreignObject')
+         .attr('x', x - 30)
+         .attr('y', y + data.Radius/2*4)
+         .attr('width', '120px')
+         .attr('height', '55px')
+         .append('xhtml:div')
+         .html(data['FullName'])
+         .attr('class', 'fullNameNode')
+         .attr('class', function() {
+             switch(data['Relationship']) {
+                case 'friend':
+                    return "fullNameBorderFriend";
+                case 'family':
+                    return 'fullNameBorderFamily';
+                default:
+                    return 'fullNameBorderBA';
+             }
+         });
+    showSummary(node);
+ }
+ 
+ /**
+  * Removes all elements of the class .searchedText from the view
+  */
+ function clearSearch() {
+     d3.selectAll('.searchedText').remove();
+     document.getElementById('searchBar').value = "";
+     clearDisplay();
+ }
+
+ /**
+  * Toggles the absolutely positioned user menu
+  */
+ function toggleMenu() {
+    if(USER_MENU_SHOWN) {
+        document.getElementById("Toggleable").style.display = "none";
+        document.getElementById("ButtonText").innerHTML = "Show Menu";
+        document.getElementById("ButtonArrow").style.transform = "rotate(0deg)";
+        document.getElementById("UserMenu").style.width = USER_MENU_WIDTH_HIDE;
+    } else {
+        document.getElementById("UserMenu").style.width = USER_MENU_WIDTH_SHOW;
+        document.getElementById("Toggleable").style.display = "block";
+        document.getElementById("ButtonText").innerHTML = "Hide Menu";
+        document.getElementById("ButtonArrow").style.transform = "rotate(180deg)";
+    }
+    USER_MENU_SHOWN = !USER_MENU_SHOWN;
+ }
